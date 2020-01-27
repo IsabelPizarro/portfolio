35fe593d0a7cf913eccbadf9ea1e597283b71274
Isabel <chabelipizarro1@gmail.com>
2020-01-27 23:33:57 +0100

    make transition in navbar

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 console.log("hola");
 
-let headerResize=document.querySelector(".data-header-resize");
-// var headerResize = $('#banner').data('header-resize');
+
+
+
+
+var headerResize = $('#banner').data('header-resize');
 
 if( headerResize == 1 ) {
 	$(window).bind('scroll', toggleNavClass);
 }
 
-// function toggleNavClass() {
-// 	var scrollTop = $(window).scrollTop();
-// 	var windowWidth = $(window).width();
-// 	$('#banner').toggleClass( 'smallbar', scrollTop > 100 );
-// }
+function toggleNavClass() {
+	var scrollTop = $(window).scrollTop();
+	var windowWidth = $(window).width();
+	$('#banner').toggleClass( 'smallbar', scrollTop > 100 );
+}
